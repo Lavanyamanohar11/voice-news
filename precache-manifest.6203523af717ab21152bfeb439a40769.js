be6aa2eafeedd3df7e12d32ba5f309b4d863a412
Lavanya M <lavanyamanohar11@gmail.com>
2021-10-28 21:44:36 +0530

    Updates

---
+++ precache-manifest.6203523af717ab21152bfeb439a40769.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b838554f493e16ac835a2274530cc59",
+    "url": "/voice-news/index.html"
+  },
+  {
+    "revision": "648400dd908e93e7c297",
+    "url": "/voice-news/static/css/main.1461b181.chunk.css"
+  },
+  {
+    "revision": "ae8997939b3d414e0522",
+    "url": "/voice-news/static/js/2.c0fef7a6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/voice-news/static/js/2.c0fef7a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "648400dd908e93e7c297",
+    "url": "/voice-news/static/js/main.7fb856b3.chunk.js"
+  },
+  {
+    "revision": "ee251f2bd0ccd9277285",
+    "url": "/voice-news/static/js/runtime-main.f74dc81e.js"
+  },
+  {
+    "revision": "561461ea4677301a674cc0d9f0d835e2",
+    "url": "/voice-news/static/media/alanLogo.561461ea.png"
+  }
+]);
